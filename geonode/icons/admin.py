1180bc4db7d28d1b524ff1c688bde1c4956a2350
chumano <xuanloc_hoang2000@yahoo.com>
2024-02-22 11:12:03 +0700

    CustomActionAdmin

--- geonode/icons/admin.py
+++ geonode/icons/admin.py
@@ -10,3 +10,28 @@ class IconAdmin(admin.ModelAdmin):
 
     def image_icon(self, obj):
         return format_html('<img width="32" src="{}" />'.format(obj.path.url))
+    
+
+# [chumano] custom actstream
+from actstream import models as amodels, admin as aadmin
+try:
+    from genericadmin.admin import GenericAdminModelAdmin as ModelAdmin
+except ImportError:
+    ModelAdmin = admin.ModelAdmin
+
+class CustomActionAdmin(ModelAdmin):
+    view_on_site = False
+    date_hierarchy = 'timestamp'
+    list_display = ('__str__', 'actor', 'verb', 'target', 'public')
+    list_editable = ('verb',)
+    list_filter = ('timestamp',)
+    raw_id_fields = ('actor_content_type', 'target_content_type',
+                     'action_object_content_type')
+    def has_change_permission(self, request, obj=None):
+        return False
+    def has_add_permission(self, request):
+        return False
+
+#print(aadmin.ActionAdmin)   
+admin.site.unregister(amodels.Action)
+admin.site.register(amodels.Action, CustomActionAdmin)
